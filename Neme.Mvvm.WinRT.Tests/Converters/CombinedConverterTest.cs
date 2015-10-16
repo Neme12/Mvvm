9f8a888a113f0f083b65f8975c4f368ca0037e7d
Neme <neme112@outlook.com>
2015-10-16 21:57:36 +0200

    CombinedConverter

---
+++ Neme.Mvvm.WinRT.Tests/Converters/CombinedConverterTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml;
+
+namespace Neme.Mvvm.Converters.Tests
+{
+    [TestClass]
+    public class CombinedConverterTest
+    {
+        private readonly CombinedConverter converter;
+
+        public CombinedConverterTest()
+        {
+            converter = new CombinedConverter();
+            converter.Add(new BooleanInverter());
+            converter.Add(new Visibilitizer());
+        }
+
+        [TestMethod]
+        public void TestConvert()
+        {
+            Assert.AreEqual(Visibility.Collapsed, converter.Convert(true, null, null, null));
+            Assert.AreEqual(Visibility.Visible, converter.Convert(false, null, null, null));
+        }
+
+        [TestMethod]
+        public void TestConvertBack()
+        {
+            Assert.AreEqual(false, converter.ConvertBack(Visibility.Visible, null, null, null));
+            Assert.AreEqual(true, converter.ConvertBack(Visibility.Collapsed, null, null, null));
+        }
+
+        [TestMethod]
+        public void TestPassedLanguage()
+        {
+            var spy = new ValueConverterSpy();
+            converter.Add(spy);
+
+            converter.Convert(true, null, null, "language");
+            Assert.AreEqual("language", spy.CalledLanguage);
+
+            converter.ConvertBack(Visibility.Visible, null, null, "language");
+            Assert.AreEqual("language", spy.CalledLanguage);
+        }
+    }
+
+    class ValueConverterSpy : IValueConverter
+    {
+        public string CalledLanguage { get; private set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            CalledLanguage = language;
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            CalledLanguage = language;
+            return value;
+        }
+    }
+}
---
+++ Neme.Mvvm.WinRT/Converters/CombinedConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Neme.Mvvm.Converters
+{
+    public class CombinedConverter : List<IValueConverter>, IValueConverter
+    {
+        private IEnumerable<IValueConverter> converters => this;
+        private IEnumerable<IValueConverter> convertersReversed => (this as IEnumerable<IValueConverter>).Reverse();
+
+        public object Convert(object value, Type targetType, object parameter, string language) =>
+            converters.Aggregate(value, (current, converter) => converter.Convert(current, null, null, language));
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language) =>
+            convertersReversed.Aggregate(value, (current, converter) => converter.ConvertBack(current, null, null, language));
+    }
+}
