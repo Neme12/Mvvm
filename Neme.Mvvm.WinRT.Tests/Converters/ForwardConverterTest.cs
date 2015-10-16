1de494a3b3ef9879e0d51af896f00115c64ae510
Neme <neme112@outlook.com>
2015-10-16 20:36:07 +0200

    Neme.Mvvm.WinRT

---
+++ Neme.Mvvm.WinRT.Tests/Converters/ForwardConverterTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace Neme.Mvvm.Converters.Tests
+{
+    [TestClass]
+    public class ForwardConverterTest
+    {
+    }
+}
