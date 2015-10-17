7df16869d257da056908bad149ae7b70ce47a4cb
Neme <neme112@outlook.com>
2015-10-17 20:47:57 +0200

    AvailabilityToVisibilityConverter

---
+++ Neme.Mvvm.WinRT.Tests/Converters/AvailabilityToVisibilityConverterTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml;
+using Neme.Mvvm.Commands;
+
+namespace Neme.Mvvm.Converters.Tests
+{
+    [TestClass]
+    public class AvailabilityToVisibilityConverterTest
+    {
+        private readonly AvailabilityToVisibilityConverter converter = new AvailabilityToVisibilityConverter();
+
+        [TestMethod]
+        public void TestConvert()
+        {
+            Assert.AreEqual(Visibility.Visible, converter.Convert(Availability.Available));
+            Assert.AreEqual(Visibility.Visible, converter.Convert(Availability.Disabled));
+            Assert.AreEqual(Visibility.Collapsed, converter.Convert(Availability.Hidden));
+        }
+    }
+}
---
+++ Neme.Mvvm.WinRT/Converters/AvailabilityToVisibilityConverter.cs
@@ -0,0 +1,16 @@
+﻿using Neme.Mvvm.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Neme.Mvvm.Converters
+{
+    public class AvailabilityToVisibilityConverter : ForwardConverter<Availability, Visibility>
+    {
+        public override Visibility Convert(Availability value) =>
+            value == Availability.Hidden ? Visibility.Collapsed : Visibility.Visible;
+    }
+}
