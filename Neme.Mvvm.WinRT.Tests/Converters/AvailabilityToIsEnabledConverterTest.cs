630ea6b467880ad68e0d6c33707744f7842479c1
Neme <neme112@outlook.com>
2015-10-17 20:52:57 +0200

    AvailabilityToIsEnabledConverter

---
+++ Neme.Mvvm.WinRT.Tests/Converters/AvailabilityToIsEnabledConverterTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml;
+using Neme.Mvvm.Commands;
+
+namespace Neme.Mvvm.Converters.Tests
+{
+    [TestClass]
+    public class AvailabilityToIsEnabledConverterTest
+    {
+        private readonly AvailabilityToIsEnabledConverter converter = new AvailabilityToIsEnabledConverter();
+
+        [TestMethod]
+        public void TestConvert()
+        {
+            Assert.AreEqual(true, converter.Convert(Availability.Available));
+            Assert.AreEqual(false, converter.Convert(Availability.Disabled));
+            Assert.AreEqual(false, converter.Convert(Availability.Hidden));
+        }
+    }
+}
---
+++ Neme.Mvvm.WinRT/Converters/AvailabilityToIsEnabledConverter.cs
@@ -0,0 +1,16 @@
+﻿using Neme.Mvvm.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Neme.Mvvm.Converters
+{
+    public class AvailabilityToIsEnabledConverter : ForwardConverter<Availability, bool>
+    {
+        public override bool Convert(Availability value) =>
+            value == Availability.Available;
+    }
+}
