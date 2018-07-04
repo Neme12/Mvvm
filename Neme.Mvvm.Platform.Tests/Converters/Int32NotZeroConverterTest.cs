dfb9cd0fe14ad5eeb41b675b605343dc08c53171
Šimon Koníček <neme112@outlook.com>
2018-07-04 17:57:45 +0200

    Adding Int32NotZeroConverter

---
+++ Neme.Mvvm.Platform.Tests/Converters/Int32NotZeroConverterTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Neme.Mvvm.Converters.Tests
+{
+    [TestClass]
+    public class Int32NotZeroConverterTest
+    {
+        private readonly Int32NotZeroConverter converter = new Int32NotZeroConverter();
+
+        [TestMethod]
+        public void TestConvert()
+        {
+            Assert.AreEqual(false, converter.Convert(0));
+            Assert.AreEqual(true, converter.Convert(-1));
+            Assert.AreEqual(true, converter.Convert(1));
+        }
+    }
+}
---
+++ Neme.Mvvm.Platform/Converters/Int32NotZeroConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neme.Mvvm.Converters
+{
+    public class Int32NotZeroConverter : OneWayConverter<int, bool>
+    {
+        public override bool Convert(int value) => value != 0;
+    }
+}
