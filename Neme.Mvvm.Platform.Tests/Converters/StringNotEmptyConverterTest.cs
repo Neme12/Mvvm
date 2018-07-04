22869dc9932179f63811faad8b31d76032c83b83
Šimon Koníček <neme112@outlook.com>
2018-07-04 17:47:43 +0200

    Adding StringNotEmptyConverter

---
+++ Neme.Mvvm.Platform.Tests/Converters/StringNotEmptyConverterTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Neme.Mvvm.Converters.Tests
+{
+    [TestClass]
+    public class StringNotEmptyConverterTest
+    {
+        private readonly StringNotEmptyConverter converter = new StringNotEmptyConverter();
+
+        [TestMethod]
+        public void TestConvert()
+        {
+            Assert.AreEqual("value", Assert.ThrowsException<ArgumentNullException>(() => converter.Convert(null)).ParamName);
+            Assert.AreEqual(false, converter.Convert(""));
+            Assert.AreEqual(true, converter.Convert("hello"));
+        }
+    }
+}
---
+++ Neme.Mvvm.Platform/Converters/StringNotEmptyConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neme.Mvvm.Converters
+{
+    public class StringNotEmptyConverter : OneWayConverter<string, bool>
+    {
+        public override bool Convert(string value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            return value.Length > 0;
+        }
+    }
+}
