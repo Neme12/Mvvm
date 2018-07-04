27fb56713a0a9e450a3b4300beb1d46369e2a5f7
Šimon Koníček <neme112@outlook.com>
2018-07-04 17:22:01 +0200

    Adding Objectifier

---
+++ Neme.Mvvm.Platform.Tests/Converters/ObjectifierTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Neme.Mvvm.Converters.Tests
+{
+    [TestClass]
+    public class ObjectifierTest
+    {
+        private readonly Objectifier converter = new Objectifier();
+
+        [TestMethod]
+        public void TestConvert()
+        {
+            Assert.AreEqual(1, converter.Convert(1));
+            Assert.AreEqual("hello", converter.Convert("hello"));
+        }
+
+        [TestMethod]
+        public void TestConvertBack()
+        {
+            Assert.AreEqual(1, converter.ConvertBack(1));
+            Assert.AreEqual("hello", converter.ConvertBack("hello"));
+        }
+    }
+}
---
+++ Neme.Mvvm.Platform/Converters/Objectifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neme.Mvvm.Converters
+{
+    public class Objectifier : TwoWayConverter<object, object>
+    {
+        public override object Convert(object value) => value;
+
+        public override object ConvertBack(object value) => value;
+    }
+}
