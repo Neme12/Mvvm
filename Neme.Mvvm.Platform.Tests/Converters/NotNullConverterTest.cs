b4c1e0e2d4fe9bf09e2ef5a3c490aa448fa14e41
Šimon Koníček <neme112@outlook.com>
2018-07-04 17:41:23 +0200

    Adding NotNullConverter

---
+++ Neme.Mvvm.Platform.Tests/Converters/NotNullConverterTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Neme.Mvvm.Converters.Tests
+{
+    [TestClass]
+    public class NotNullConverterTest
+    {
+        private readonly NotNullConverter converter = new NotNullConverter();
+
+        [TestMethod]
+        public void TestConvert()
+        {
+            Assert.AreEqual(false, converter.Convert(null));
+            Assert.AreEqual(true, converter.Convert(""));
+            Assert.AreEqual(true, converter.Convert(1));
+        }
+    }
+}
---
+++ Neme.Mvvm.Platform/Converters/NotNullConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neme.Mvvm.Converters
+{
+    public class NotNullConverter : OneWayConverter<object, bool>
+    {
+        public override bool Convert(object value) => value != null;
+    }
+}
