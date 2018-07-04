e0fbc06bb20cdfbee0d11356e60bb6ccd91b8f13
Šimon Koníček <neme112@outlook.com>
2018-07-04 17:14:27 +0200

    Adding Multiplier

---
+++ Neme.Mvvm.Platform.Tests/Converters/MultiplierTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Neme.Mvvm.Converters.Tests
+{
+    [TestClass]
+    public class MultiplierTest
+    {
+        private readonly Multiplier converter = new Multiplier { Factor = 2 };
+
+        [TestMethod]
+        public void TestConvert()
+        {
+            Assert.AreEqual(6, converter.Convert(3));
+        }
+
+        [TestMethod]
+        public void TestConvertBack()
+        {
+            Assert.AreEqual(1.5, converter.ConvertBack(3));
+        }
+    }
+}
---
+++ Neme.Mvvm.Platform/Converters/Multiplier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neme.Mvvm.Converters
+{
+    public class Multiplier : TwoWayConverter<double, double>
+    {
+        public double Factor { get; set; }
+
+        public override double Convert(double value) => value * Factor;
+
+        public override double ConvertBack(double value) => value / Factor;
+    }
+}
